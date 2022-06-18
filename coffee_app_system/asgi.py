8ddeb0a15d974755ea8e294315cbc50091888a75
Ishfak <mdishfakur12@gmail.com>
2022-06-18 20:51:05 +0600

    updated

--- coffee_app_system/asgi.py
+++ coffee_app_system/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for coffee_app_systems project.
+ASGI config for coffee_app_system project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_app_systems.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_app_system.settings')
 
 application = get_asgi_application()
--- coffee_app_system/wsgi.py
+++ coffee_app_system/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for coffee_app_systems project.
+WSGI config for coffee_app_system project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_app_systems.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'coffee_app_system.settings')
 
 application = get_wsgi_application()
